d8d20fca1ae438aaa968c32a22f6fb6c9051de47
Sandeep singh <singhsandeep09169@gmail.com>
2021-12-01 21:17:36 +0530

    Add project files.

---
+++ CalculatorTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Calculator;
+
+namespace Testing_Day1
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void AddMethodtest()
+        {
+            Calculate cobj = new Calculate();
+            int result = cobj.Add(10, 20);
+            Assert.That(result, Is.EqualTo(80));
+        }
+        [Test]
+        [TestCase(15,35,50)]
+        [TestCase(10,45,55)]
+        [TestCase(20,50,70)]
+        public void AddMethodtest(int num1,int num2,int expected)
+        {
+            Calculate cobj = new Calculate();
+            int result = cobj.Add(num1, num2);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
